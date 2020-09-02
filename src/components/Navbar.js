f438216aede67f5f094cf9705951fd50d1f44786
Svetlana <svezan@mail.ru>
2020-09-02 12:55:06 +0300

    feat: add navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import NavbarWrapper from './NavbarWrapper';
+
+import shape from '../picture/shape.svg'
+import logo from '../picture/logo.svg'
+
+const Navbar = props => {
+  return (
+    <NavbarWrapper>
+        <div className="navbar-brand">
+          <img src={shape} alt="shape" />
+          <div className="navbar-brand-text">
+            <h1>Point-X</h1>
+            Blockchain<br />Rewards Program
+          </div>
+        </div>
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <NavLink exact to="/" className="nav-link">Tasks campaigns</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/items" className="nav-link">
+              Goods items
+              <span className="goodItems">{props.goodItems || 41}</span> 
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/balance" className="nav-link">Balance  <span className="balance">{props.balance || 0} Px</span></NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/support" className="nav-link">Support Center</NavLink>
+          </li>
+        </ul>
+        <div className="navbar-name">
+          <img src={logo} alt="logo" />
+          <div className="navbar-name-text">
+            DrBeats inc
+            <br />
+            <span className="navbar-name-status">Administrator</span>
+          </div>
+        </div>
+    </NavbarWrapper>
+  );
+};
+
+export default Navbar;
---
+++ src/components/NavbarWrapper.js
@@ -0,0 +1,94 @@
+import styled from 'styled-components';
+
+const NavbarWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 96px;
+
+  .navbar-brand {
+    display: flex;
+    width: 10%;
+    img {
+      margin-right: 12px;
+    }
+    .navbar-brand-text {
+      text-align: initial;
+      font-family: Trebuchet MS;
+      font-style: normal;
+      font-weight: bold;
+      font-size: 9px;
+      line-height: 10px;
+      letter-spacing: 0.021em;
+      h1 {
+        font-size: 16px;
+        line-height: 19px;
+        letter-spacing: 0.005em;
+        color: #000000;
+        margin: 0;
+      }
+    }
+  }
+
+  .navbar-nav {
+    display: flex;
+    width: 75%;
+    .nav-item {
+      list-style-type: none;
+      margin-right: 30px;
+      a {
+        text-decoration: none;
+        cursor: pointer;
+        font-family: Inter;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 16px;
+        line-height: 19px;
+        color: #A2ACC0;
+      }
+      .balance {
+        color: #336BEC;
+      }
+      .goodItems {
+        color: #FFFFFF;
+        font-family: Inter;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 12px;
+        line-height: 15px;
+        width: 34px;
+        height: 24px;
+        background: #FF2C9C;
+        border-radius: 100px;
+      }
+      .active {
+        color: #0056D3;
+      }
+    }
+  }
+
+  .navbar-name {
+    display: flex;
+    width: 15%;
+    img {
+      margin-right: 17px;
+    }
+    .navbar-name-text {
+      font-family: Inter;
+      font-style: normal;
+      font-weight: 600;
+      font-size: 14px;
+      line-height: 17px;
+      color: #1E266D;
+      .navbar-name-status {
+        font-size: 11px;
+        line-height: 13px;
+        color: #A2ACC0;
+      }
+    }
+  }
+
+
+  `;
+
+export default NavbarWrapper;
